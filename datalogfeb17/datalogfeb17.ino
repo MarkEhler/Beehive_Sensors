6ade036d075c5606fd2d86ec1078dcd5748cfb07
MarkEhler <ehlerm42@gmail.com>
2020-02-19 09:57:47 -0500

    learning

---
+++ datalogfeb17/datalogfeb17.ino
@@ -0,0 +1,96 @@
+/*
+ * Rui Santos 
+ * Complete Project Details http://randomnerdtutorials.com
+ */
+
+#include <SPI.h> //for the SD card module
+#include <SD.h> // for the SD card
+//#include <DHT.h> // for the DHT sensor
+#include <RTClib.h> // for the RTC
+
+const int chipSelect = 10; 
+
+// Create a file to store the data
+File myFile;
+
+// RTC
+RTC_DS1307 rtc;
+
+
+void setup() {
+
+  //initializing Serial monitor
+  Serial.begin(9600);
+
+  bool parse = false;
+  bool config = false;
+  // setup for the RTC
+  rtc.begin();
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+  
+  if(!rtc.isrunning()) {
+    Serial.println("RTC is NOT running...setting time");
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+    
+  // setup for the SD card
+  Serial.print("Initializing SD card...");
+
+  if(!SD.begin(chipSelect)) {
+    Serial.println("initialization failed!");
+    return;
+  }
+  Serial.println("initialization done.");
+    
+  //open file
+  myFile=SD.open("DATA.txt", FILE_WRITE);
+
+  // if the file opened ok, write to it:
+  if (myFile) {
+    Serial.println("File opened ok");
+    // print the headings for our data
+    myFile.println("Date,Time,Temperature ºC");
+  }
+  myFile.close();
+}
+
+void loggingTime() {
+  DateTime now = rtc.now();
+  myFile = SD.open("DATA.txt", FILE_WRITE);
+  if (myFile) {
+    myFile.print(now.year(), DEC);
+    myFile.print('/');
+    myFile.print(now.month(), DEC);
+    myFile.print('/');
+    myFile.print(now.day(), DEC);
+    myFile.print(',');
+    myFile.print(now.hour(), DEC);
+    myFile.print(':');
+    myFile.print(now.minute(), DEC);
+    myFile.print(':');
+    myFile.print(now.second(), DEC);
+    myFile.print(",");
+  }
+  Serial.print(now.year(), DEC);
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.println(now.day(), DEC);
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.println(now.second(), DEC);
+  myFile.close();
+  delay(1000);  
+}
+  
+  
+
+void loop() {
+  loggingTime();
+  delay(5000);
+}
--- lora_radio/lora_radio.ino
+++ lora_radio/lora_radio.ino
@@ -1,87 +1,29 @@
-#include <RadioHead.h>
-#include <RHCRC.h>
-#include <RHDatagram.h>
-#include <RHEncryptedDriver.h>
-#include <RHGenericDriver.h>
-#include <RHGenericSPI.h>
-#include <RHHardwareSPI.h>
-#include <RHMesh.h>
-#include <RHNRFSPIDriver.h>
-#include <RHReliableDatagram.h>
-#include <RHRouter.h>
-#include <RHSoftwareSPI.h>
-#include <RHSPIDriver.h>
-#include <RHTcpProtocol.h>
-#include <RH_ASK.h>
-#include <RH_CC110.h>
-#include <RH_E32.h>
-#include <RH_MRF89.h>
-#include <RH_NRF24.h>
-#include <RH_NRF51.h>
-#include <RH_NRF905.h>
-#include <RH_RF22.h>
-#include <RH_RF24.h>
-#include <RH_RF69.h>
-#include <RH_RF95.h>
-#include <RH_Serial.h>
-#include <RH_TCP.h>
+// It is designed to work with the other example Arduino9x_TX
+ 
+#include <SPI.h>
+// #include <RH_RF95.h>
+ 
+ #define RFM95_CS 10
+ #define RFM95_RST 9
+ #define RFM95_INT 2
+ 
+// Change to 434.0 or other frequency, must match RX's freq!
+ #define RF95_FREQ 868.0
+ 
+// Singleton instance of the radio driver
+// RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
-#include <dht_nonblocking.h>
-
-//#define RF95_CS 10
-//#define RF95_INT 2
-//#define RF95_RST 9
-
-//#define RF95_FREQ 868.0
-#define DHT_SENSOR_TYPE DHT_TYPE_11
-#define TEMP 0
-
-DHT_nonblocking dht_sensor( TEMP, DHT_SENSOR_TYPE );
-//RH_RF95 rf95(RF95_CS, RF95_INT);
-
-//uint8_t data[RH_RF95_MAX_MESSAGE_LEN];
-
-void setup( )
-{
-  Serial.begin( 9600);
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  Serial.begin(9600);
+  delay(100);
 }
 
-
-
-/*
- * Poll for a measurement, keeping the state machine alive.  Returns
- * true if a measurement is available.
- */
-static bool measure_environment( float *temperature, float *humidity )
-{
-  static unsigned long measurement_timestamp = millis( );
-
-  /* Measure once every four seconds. */
-  if( millis( ) - measurement_timestamp > 3000ul )
-  {
-    if( dht_sensor.measure( temperature, humidity ) == true )
-    {
-      measurement_timestamp = millis( );
-      return( true );
-    }
-  }
-
-  return( false );
-}
-
-void loop( )
-{
-  float temperature;
-  float humidity;
-
-  /* Measure temperature and humidity.  If the functions returns
-     true, then a measurement is available. */
-  if( measure_environment( &temperature, &humidity ) == true )
-  {
-    Serial.print( "T = " );
-    Serial.print( temperature, 1 );
-    Serial.print( " deg. C, H = " );
-    Serial.print( humidity, 1 );
-    Serial.println( "%" );
-  }
+// the loop function runs over and over again forever
+void loop() {
+  Serial.print("test");   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+ // digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
 }
